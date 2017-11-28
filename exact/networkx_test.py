38da977bcbfc2e46e291e1532ce2274ecb0367c6
michelevantini <vantinimichele95@gmail.com>
2017-11-28 12:37:34 +0200

    up to eccentricity

--- exact/networkx_test.py
+++ exact/networkx_test.py
@@ -44,25 +44,35 @@ for file in graph_files:
 		largset_cc_pos = cc_sizes[-1]		
 		largest_cc = cc_list[largset_cc_pos]
 		allfiles_largest_cc_lst.append(largest_cc)
+		print("largest cc completed")
 
 		scc_sizes = np.argsort([g.size() for g in scc_list])
 		largset_scc_pos = scc_sizes[-1]		
 		largest_scc = scc_list[largset_scc_pos]
 		allfiles_largest_scc_lst.append(largest_scc)
+		print("largest scc completed")
 
 		# largest cc/scc len
 		largest_cc_len = len(largest_cc)
 		allfiles_largest_cc_len_lst.append(largest_cc_len)
+		print("largest cc len completed")
 
 		largest_scc_len = len(largest_scc)
 		allfiles_largest_scc_len_lst.append(largest_scc_len)
+		print("largest scc len completed")
 
+		largest_cc_ecc = nx.eccentricity(largest_cc)
+		largest_scc_ecc = nx.eccentricity(largest_scc)
+		'''
 		# largest cc/scc diameter
 		largest_cc_diam = nx.diameter(largest_cc)
 		allfiles_largest_cc_diam_lst.append(largest_cc_diam)
+		print("largest cc diameter completed - ", largest_cc_diam)
 
 		largest_scc_diam = nx.diameter(largest_scc)
-		largest_scc_diam_lst.append(largest_scc_diam)
+		allfiles_largest_scc_diam_lst.append(largest_scc_diam)
+		print("largest scc diameter completed - ", largest_scc_diam)
+		'''
 
 print(allfiles_cc_lst)
 print(allfiles_scc_lst)
